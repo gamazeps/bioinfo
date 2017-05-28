3b3cfc55a0331a6ad4973161b260950c211abc99
Felix Raimundo <gamaz3ps@gmail.com>
2017-05-28 14:15:07 +0200

    Add parse and better random walk

--- class2/week_2.py
+++ class2/week_2.py
@@ -4,29 +4,61 @@ def eulerian_cycle(graph):
     unexplored_edges = set()
     unexplored_nodes = set()
 
+    if len(graph) is 0:
+        return list()
+
     for k in graph:
-        for e in graph[k]:
-            unexplored_edges.add((k, e))
+        for elem in graph[k]:
+            unexplored_edges.add((k, elem))
 
     def random_walk(origin):
         path = list()
         path.append(origin)
         curr = origin
-        n = None
+        next_node = None
 
-        while n is not origin:
-            curr = n
-            n = graph[curr].pop()
+        while len(graph[curr]) is not 0:
+            print(curr)
+            next_node = graph[curr].pop()
             if len(graph[curr]) is not 0:
                 unexplored_nodes.add(curr)
-            unexplored_nodes.remove((curr, n))
-            path.append(n)
+            unexplored_edges.remove((curr, next_node))
+            path.append(next_node)
+            curr = next_node
 
         return path
 
-    first_iter_node = graph.keys()[0]
-    res = random_walk(first_iter_node)
+    # Gets a random key
+    (first_iter_node, values) = graph.popitem()
+    graph[first_iter_node] = values
+
+    result = random_walk(first_iter_node)
 
     while len(unexplored_edges) is not 0:
-        curr_path = random_walk(unexplored_nodes.pop())
+        new_origin = unexplored_nodes.pop()
+        cycle = random_walk(new_origin)
 
+    return result
+
+    #while len(unexplored_edges) is not 0:
+    #    curr_path = random_walk(unexplored_nodes.pop())
+
+def parse_input(fname):
+    workfile = open(fname, 'r')
+    lines = workfile.readlines()
+    lines = map(lambda x: x.strip(), lines)
+
+    graph = collections.defaultdict(set)
+    for line in lines:
+        pair = line.split(' -> ')
+        for elem in pair[1].split(','):
+            graph[int(pair[0])].add(int(elem))
+
+    return graph
+
+def main():
+    input_graph = parse_input('input.txt')
+    print(eulerian_cycle(input_graph))
+
+if __name__ == '__main__':
+    main()
